6563d4e4cfa97987b2cc583f790920b1c739f58f
Fredrik Dahlberg <fd@abc.se>
2018-03-03 20:17:54 +0100

    Add gender handling.

--- object.py
+++ object.py
@@ -2,33 +2,66 @@ import uuid
 import random
 random.seed()
 
+FEMALE = 0
+MALE = 1
+
 def coinflip():
-	return random.randint(0, 1)
+    return random.randint(0, 1)
 	
 class Human():
-	id = None
-	father = None
-	mother = None
-	generation = None
-	genes = None
-	children = None
+    id = None
+    father = None
+    mother = None
+    generation = None
+    genes = None
+    children = None
+    gender = None
 
-	def __init__(self, father=None, mother=None):
-		self.id = uuid.uuid4()
-		self.genes = [None] * 23
-		self.children = []
-		if father is None and mother is None:
-			for pair in range(0, 22):
-				self.genes[pair] = (uuid.uuid4(), uuid.uuid4())
-			self.generation = 0
-		else:
-			if mother.generation > father.generation:
-				self.generation = mother.generation + 1
-			else:
-				self.generation = father.generation + 1
-			for pair in range(0, 22):
-				self.genes[pair] = (father.genes[pair][coinflip()], mother.genes[pair][coinflip()])
-			self.father = father.id
-			self.mother = mother.id
-			father.children.append(self.id)
-			mother.children.append(self.id)
+    def __init__(self, mother=None, father=None, gender=None):
+        self.id = uuid.uuid4()
+        self.genes = [None] * 23
+        self.children = []
+        if father is None and mother is None:
+            for pair in range(0, 23):
+                self.genes[pair] = [int(uuid.uuid4()), int(uuid.uuid4())]
+            self.genes[22][0] = self.genes[22][0] & (~0 << 1)
+            self.generation = 0
+            if gender is not None:
+                self.genes[22][1] = self.genes[22][1] & (~0 << 1) | gender
+#                self.genes[22][1] = self.genes[22][1] & gender
+#                if gender == MALE:
+#                    self.genes[22][1] = self.genes[22][1] | 1
+#                else:
+#                    self.genes[22][1] = self.genes[22][1] & (~0 << 1)
+        else:
+            if mother.generation > father.generation:
+                self.generation = mother.generation + 1
+            else:
+                self.generation = father.generation + 1
+            for pair in range(0, 23):
+                self.genes[pair] = [mother.genes[pair][coinflip()], father.genes[pair][coinflip()]]
+            if gender is not None:
+                self.genes[22][1] = father.genes[22][gender]
+#                    if father.genes[22][0] & gender:
+#                        self.genes[22][1] = father.genes[22][0]
+#                    else if father.genes[22][1] & gender:
+#                        self.genes[22][1] = 
+                        
+            self.father = father.id
+            self.mother = mother.id
+            father.children.append(self.id)
+            mother.children.append(self.id)
+        self.setgender()
+
+    def setgender(self):
+        if (self.genes[22][0] & 1) | (self.genes[22][1] & 1):
+            self.gender = MALE
+        else:
+            self.gender = FEMALE
+
+    def inbred(self):
+        inbred = 0
+        for i in self.genes:
+            if i[0] == i[1]:
+                inbred += 1
+        return inbred
